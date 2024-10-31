5c51791a1df8c2be546c578eb7a710805f926a8c
MT Nahill <mt@evitalin.com>
2024-10-31 17:53:34 +0700

    add tutorial security

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'estate',
-    'depends': [
-        'base',
+    "name": "estate",
+    "depends": [
+        "base",
     ],
-    'application': True,
-    'installable': True,
+    "application": True,
+    "installable": True,
+    "data": [
+        "security/ir.model.access.csv",
+    ],
 }
